770c5688e40f31e4450b6e4e012d242d05fd98fa
tulio-amaral <tuliosantos@gee.inatel.br>
2021-05-28 16:25:11 -0300

    feat: add error handling

--- src/framework/http/app.ts
+++ src/framework/http/app.ts
@@ -1,5 +1,23 @@
-import express from 'express';
+import express, { json, NextFunction, Request, Response } from 'express';
+
+import AppError from './errors/AppError';
 
 const app = express();
+
+app.use(json());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
 
 export default app;
