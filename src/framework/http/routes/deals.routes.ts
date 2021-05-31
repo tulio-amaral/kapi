203a162bf5d7af772489c7c2b14e77c683665789
tulio-amaral <tuliosantos@gee.inatel.br>
2021-05-31 17:27:49 -0300

    feat: add routes to list all deals

--- src/framework/http/routes/deals.routes.ts
+++ src/framework/http/routes/deals.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 
 import CreateDealsController from '../controller/CreateDealsController';
+import GroupDealByDayController from '../controller/GroupDealByDayController';
+import ListDealsController from '../controller/ListDealsController';
 
 const dealsRoutes = Router();
 
 const createDealsController = new CreateDealsController();
+const groupDealByDayController = new GroupDealByDayController();
+const listDealsController = new ListDealsController();
 
 dealsRoutes.post('/', createDealsController.handle);
+dealsRoutes.get('/', groupDealByDayController.handle);
+
+dealsRoutes.get('/list', listDealsController.handle);
 
 export default dealsRoutes;
