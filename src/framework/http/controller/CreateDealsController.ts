bbf84570d60edd25300e2f49f85c8956b3175a77
tulio-amaral <tuliosantos@gee.inatel.br>
2021-06-01 09:07:05 -0300

    feat: add create deal at bling use case to controller

--- src/framework/http/controller/CreateDealsController.ts
+++ src/framework/http/controller/CreateDealsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 
+import CreateDealsAtBlingUseCase from '../../../useCases/CreateDealsAtBlingUseCase';
 import CreateDealsUseCase from '../../../useCases/CreateDealsUseCase';
 import ListDealsAtPipedriveUseCase from '../../../useCases/ListDealsAtPipedriveUseCase';
 
@@ -7,8 +8,23 @@ class CreateDealsController {
   async handle(request: Request, response: Response): Promise<Response> {
     const listDealsAtPipedriveUseCase = new ListDealsAtPipedriveUseCase();
     const createDealsUseCase = new CreateDealsUseCase();
+    // const createDealAtBlingUseCase = new CreateDealsAtBlingUseCase();
 
     const listDeals = await listDealsAtPipedriveUseCase.execute();
+
+    // Add deal to Bling (not working, although all parameters are being sent)
+
+    // try {
+    //   listDeals.map(deal => {
+    //     return createDealAtBlingUseCase.execute({
+    //       name: deal.name,
+    //       sequence: deal.sequence,
+    //       value: deal.value,
+    //     });
+    //   });
+    // } catch {
+    //   return response.send();
+    // }
 
     listDeals.map(deal => {
       return createDealsUseCase.execute({
